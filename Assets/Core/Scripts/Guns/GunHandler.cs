e455d06c2d755d00fa2e7f54728e23c18500383b
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-09-18 09:53:05 +0200

    Gun generator

---
+++ Assets/Core/Scripts/Guns/GunHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GunHandler : MonoBehaviour
+{
+    GunHandler() { m_ActiveGunIdx = -1; }
+
+    #region design vars
+    public Transform m_CameraTransform;     // Acts as weapon root transform
+
+    [Header("Gun Locker")]
+    public int m_DefaultGun;
+    public GameObject[] m_ProjectileGunVariantPrefabs;
+    #endregion
+
+    private GameObject[] m_ProjectileGunClones;
+    private GameObject m_ActiveGun;
+    private GunTemplate m_ActiveGunData;
+    private int m_ActiveGunIdx;
+
+
+    //----------------------------------------------------------------------------------------------------
+
+
+    public GameObject GetActiveGun()
+    {
+        return m_ActiveGun;
+    }
+
+
+    public void SetActiveGun(int idx)
+    {
+        if(idx != m_ActiveGunIdx)
+        {
+            m_ActiveGunIdx = idx;
+
+            if(m_ActiveGun != null)
+                m_ActiveGun.SetActive(false);
+
+            m_ActiveGun = m_ProjectileGunClones[m_ActiveGunIdx];
+            m_ActiveGun.SetActive(true);
+            m_ActiveGunData = m_ActiveGun.GetComponent<GunTemplate>();
+        }
+    }
+
+
+    private void CreateGunInstances()
+    {
+        m_ProjectileGunClones = new GameObject[m_ProjectileGunVariantPrefabs.Length];
+        for(int i = 0; i < m_ProjectileGunVariantPrefabs.Length; ++i)
+        {
+            m_ProjectileGunClones[i] = Instantiate(m_ProjectileGunVariantPrefabs[i], Vector3.zero, Quaternion.identity);
+            m_ProjectileGunClones[i].SetActive(false);
+        }
+    }
+
+
+    void Start()
+    {
+        CreateGunInstances();
+        SetActiveGun(m_DefaultGun);
+    }
+
+
+    void Update()
+    {
+        m_ActiveGunData.m_GunData.Rotation = m_CameraTransform.rotation;
+        m_ActiveGunData.m_GunData.Position = m_CameraTransform.position;
+    }
+}
---
+++ Assets/Core/Scripts/Guns/GunTemplate.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GunTemplate : MonoBehaviour
+{
+    #region design vars
+    [Header("Object Data")]
+    public GameObject m_GunModelPrefab;
+    public Vector3 m_Position;
+    public Vector3 m_Rotation;
+
+    [Header("Properties")]
+    public float m_Speed;
+    // bullet
+    // vfx
+    #endregion
+
+    private GameObject m_GunModel;
+    [HideInInspector]
+    public GunData m_GunData;
+
+
+    public struct GunData
+    {
+        public Quaternion Rotation;
+        public Vector3 Position;
+        public float Speed;
+    }
+
+
+    //----------------------------------------------------------------------------------------------------
+
+
+    public GameObject GetGunModel()
+    {
+        return m_GunModel;
+    }
+
+
+    private void Start()
+    {
+        m_GunData.Rotation = Quaternion.LookRotation(m_Rotation, Vector3.up);
+        m_GunData.Position = m_Position;
+        m_GunData.Speed = m_Speed;
+
+        m_GunModel = Instantiate(m_GunModelPrefab, m_Position, Quaternion.identity);
+    }
+
+
+    private void Update()
+    {
+        m_GunModel.transform.position = m_GunData.Position + m_Position;
+        m_GunModel.transform.rotation = m_GunData.Rotation;
+    }
+}
