3de4dce8d7d885e641b271b4d74ac1987508b621
ChickenMechanics <54860211+ChickenMechanics@users.noreply.github.com>
2019-09-20 10:30:48 +0200

    Bullet lifetime

--- Assets/Core/Scripts/Bullets/BulletBehaviour.cs
+++ Assets/Core/Scripts/Bullets/BulletBehaviour.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BulletBehaviour : MonoBehaviour
 {
     #region design vars
@@ -10,30 +11,34 @@ public class BulletBehaviour : MonoBehaviour
     public float m_FireRate;
     public float m_Speed;
     public float m_Gravity;
+    public float m_MaxLifetimeInSec;
     // bullet
     // vfx
     #endregion
-
-    // Bullet things
+    
     private GameObject m_BulletModel;
     private BulletBehaviour m_BulletBehaviour;
     private Transform m_BulletSpawnPoint;
     private Vector3 m_Dir;
+    private float m_CurrentLifeTime;
 
 
     public void InitBullet()
     {
         m_BulletModel = Instantiate(m_BulletModelPrefab, Vector3.zero, Quaternion.identity);
         m_BulletModel.transform.SetParent(transform);
+
+        m_CurrentLifeTime = 0.0f;
     }
 
 
     public void Fire(Transform spawnPoint, Vector3 dir)
     {
-        m_BulletModel.transform.position = spawnPoint.position;
-        m_BulletModel.transform.rotation = spawnPoint.rotation;
-
+        m_BulletSpawnPoint = spawnPoint;
         m_Dir = dir;
+
+        m_BulletModel.transform.position = m_BulletSpawnPoint.position;
+        m_BulletModel.transform.rotation = m_BulletSpawnPoint.rotation;
 
         gameObject.SetActive(true);
     }
@@ -67,6 +72,15 @@ public class BulletBehaviour : MonoBehaviour
     private void FixedUpdate()
     {
         transform.position += ((m_Dir * m_Speed) + new Vector3(0.0f, m_Gravity, 0.0f)) * Time.deltaTime;
+
+        Debug.Log(m_CurrentLifeTime);
+        m_CurrentLifeTime += Time.deltaTime;
+
+        if (m_CurrentLifeTime > m_MaxLifetimeInSec)
+        {
+            
+            Destroy(this);
+        }
 
         //TODO: Destroy after max range and walls and enemies and all that
     }
